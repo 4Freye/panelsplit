d6a653de0eb7e3056690482d8feb352ebd14d4c9
Eric Frey <4freye@gmail.com>
2024-01-11 13:14:02 -0500

    Update panelsplit.py

--- panelsplit/panelsplit.py
+++ panelsplit/panelsplit.py
@@ -1,6 +1,6 @@
 from sklearn.model_selection import TimeSeriesSplit
 from tqdm import tqdm
-import numpy as np
+import pandas as pd
 
 class PanelSplit:
     def __init__(self, unique_periods, train_periods, n_splits=5, gap=None, test_size=None, max_train_size=None):
@@ -11,12 +11,12 @@ class PanelSplit:
         - n_splits: Number of splits for TimeSeriesSplit
         - gap: Gap between train and test sets in TimeSeriesSplit
         - test_size: Size of the test set in TimeSeriesSplit
-        - unique_periods: NumPy array or list containing unique periods
+        - unique_periods: Pandas DataFrame or Series containing unique periods
         - train_periods: All available training periods
         - max_train_size: Maximum size for a single training set.
         """
         self.tss = TimeSeriesSplit(n_splits=n_splits, gap=gap, test_size=test_size, max_train_size=max_train_size)
-        indices = list(self.tss.split(unique_periods))
+        indices = self.tss.split(unique_periods)
         self.u_periods_cv = self.split_unique_periods(indices, unique_periods)
         self.all_periods = train_periods
         self.n_splits = n_splits
@@ -27,14 +27,14 @@ class PanelSplit:
 
         Parameters:
         - indices: TimeSeriesSplit indices
-        - unique_periods: NumPy array or list containing unique periods
+        - unique_periods: Pandas DataFrame or Series containing unique periods
 
         Returns: List of tuples containing train and test periods
         """
         u_periods_cv = []
         for i, (train_index, test_index) in enumerate(indices):
-            unique_train_periods = unique_periods[train_index]
-            unique_test_periods = unique_periods[test_index]
+            unique_train_periods = unique_periods.iloc[train_index].values
+            unique_test_periods = unique_periods.iloc[test_index].values
             u_periods_cv.append((unique_train_periods, unique_test_periods))
         return u_periods_cv
 
@@ -45,8 +45,8 @@ class PanelSplit:
         self.all_indices = []
 
         for i, (train_periods, test_periods) in enumerate(self.u_periods_cv):
-            train_indices = np.where(np.isin(self.all_periods, train_periods))[0]
-            test_indices = np.where(np.isin(self.all_periods, test_periods))[0]
+            train_indices = self.all_periods.loc[self.all_periods.isin(train_periods)].index
+            test_indices = self.all_periods.loc[self.all_periods.isin(test_periods)].index
             self.all_indices.append((train_indices, test_indices))
 
         return self.all_indices
@@ -56,58 +56,57 @@ class PanelSplit:
         Returns: Number of splits
         """
         return self.n_splits
-    
+
     def cross_val_predict(self, estimator, X, y, indices, prediction_method='predict'):
         """
         Perform cross-validated predictions using a given predictor model.
-    
+
         Parameters:
         -----------
         estimator : The machine learning model used for prediction.
-    
-        X : NumPy array
+
+        X : pandas DataFrame
             The input features for the predictor.
-    
-        y : NumPy array
+
+        y : pandas Series
             The target variable to be predicted.
-    
-        indices : NumPy array
+
+        indices : pandas DataFrame
             Indices corresponding to the dataset.
-    
+
         prediction_method : str, optional (default='predict')
             The prediction method to use. It can be 'predict', 'predict_proba', or 'predict_log_proba'.
-    
+
         Returns:
         --------
-        dict
-            Dictionary containing predictions made by the model during cross-validation.
-            Keys are indices, and values are the corresponding true target values.
+        pd.DataFrame
+            Concatenated DataFrame containing predictions made by the model during cross-validation.
+            It includes the original indices joined with the predicted values.
+
         """
-        predictions = {}
-    
-        for train_indices, test_indices in tqdm(self.split(X=X, y=y)):
-            # Remove NaN values from y_train
-            train_mask = ~np.isnan(y[train_indices])
-            y_train = y[train_indices][train_mask]
-            X_train = X[train_indices][train_mask]
-    
-            X_test, y_test = X[test_indices], y[test_indices]
-    
+        predictions = []
+
+        for train_indices, test_indices in tqdm(self.all_indices):
+            # first drop nas with respect to y_train
+            y_train = y.iloc[train_indices].dropna()
+            # use y_train to filter for X_train
+            X_train = X.iloc[y_train.index]
+            X_test, y_test = X.iloc[test_indices], y.iloc[test_indices]
+
+            pred = indices.iloc[test_indices].copy()
+            pred['y_true'] = y_test.values
+
             model = estimator.fit(X_train, y_train)
-    
+
             if prediction_method == 'predict':
-                y_pred = model.predict(X_test)
+                pred['y_pred'] = model.predict(X_test)
             elif prediction_method == 'predict_proba':
-                y_pred = model.predict_proba(X_test)[:, 1]
+                pred['y_pred'] = model.predict_proba(X_test)[:, 1]
             elif prediction_method == 'predict_log_proba':
-                y_pred = model.predict_log_proba(X_test)[:, 1]
+                pred['y_pred'] = model.predict_log_proba(X_test)[:, 1]
             else:
                 raise ValueError("Invalid prediction_method. Supported values are 'predict', 'predict_proba', or 'predict_log_proba'.")
-    
-            # Convert test indices to a list for cases where test_indices is a NumPy array
-            test_indices_list = test_indices.tolist()
-    
-            # Update predictions dictionary with key-value pairs
-            predictions.update(dict(zip(test_indices_list, y_test)))
-    
-        return predictions
+
+            predictions.append(pred)
+
+        return pd.concat(predictions, axis=0)
