08d8ba7908b09210c8fb1ff19b366a63b0ca50d1
Eric Frey <4freye@gmail.com>
2024-01-11 12:42:26 -0500

    Update panelsplit.py

--- panelsplit/panelsplit.py
+++ panelsplit/panelsplit.py
@@ -1,36 +1,16 @@
 from sklearn.model_selection import TimeSeriesSplit
 from tqdm import tqdm
-import pandas as pd
+import numpy as np
 
 class PanelSplit:
     def __init__(self, unique_periods, train_periods, n_splits=5, gap=None, test_size=None, max_train_size=None):
-        """
-        A class for performing time series cross-validation with custom train/test splits based on unique periods.
-
-        Parameters:
-        - n_splits: Number of splits for TimeSeriesSplit
-        - gap: Gap between train and test sets in TimeSeriesSplit
-        - test_size: Size of the test set in TimeSeriesSplit
-        - unique_periods: Pandas DataFrame or Series containing unique periods
-        - train_periods: All available training periods
-        - max_train_size: Maximum size for a single training set.
-        """
         self.tss = TimeSeriesSplit(n_splits=n_splits, gap=gap, test_size=test_size, max_train_size=max_train_size)
-        indices = self.tss.split(unique_periods)
+        indices = list(self.tss.split(unique_periods))
         self.u_periods_cv = self.split_unique_periods(indices, unique_periods)
         self.all_periods = train_periods
         self.n_splits = n_splits
 
     def split_unique_periods(self, indices, unique_periods):
-        """
-        Split unique periods into train/test sets based on TimeSeriesSplit indices.
-
-        Parameters:
-        - indices: TimeSeriesSplit indices
-        - unique_periods: Pandas DataFrame or Series containing unique periods
-
-        Returns: List of tuples containing train and test periods
-        """
         u_periods_cv = []
         for i, (train_index, test_index) in enumerate(indices):
             unique_train_periods = unique_periods.iloc[train_index].values
@@ -39,62 +19,27 @@ class PanelSplit:
         return u_periods_cv
 
     def split(self, X=None, y=None, groups=None):
-        """
-        Generate train/test indices based on unique periods.
-        """
         self.all_indices = []
 
         for i, (train_periods, test_periods) in enumerate(self.u_periods_cv):
-            train_indices = self.all_periods.loc[self.all_periods.isin(train_periods)].index
-            test_indices = self.all_periods.loc[self.all_periods.isin(test_periods)].index
+            train_indices = np.where(np.isin(self.all_periods, train_periods))[0]
+            test_indices = np.where(np.isin(self.all_periods, test_periods))[0]
             self.all_indices.append((train_indices, test_indices))
 
         return self.all_indices
 
     def get_n_splits(self, X=None, y=None, groups=None):
-        """
-        Returns: Number of splits
-        """
         return self.n_splits
 
     def cross_val_predict(self, estimator, X, y, indices, prediction_method='predict'):
-        """
-        Perform cross-validated predictions using a given predictor model.
-
-        Parameters:
-        -----------
-        estimator : The machine learning model used for prediction.
-
-        X : pandas DataFrame
-            The input features for the predictor.
-
-        y : pandas Series
-            The target variable to be predicted.
-
-        indices : pandas DataFrame
-            Indices corresponding to the dataset.
-
-        prediction_method : str, optional (default='predict')
-            The prediction method to use. It can be 'predict', 'predict_proba', or 'predict_log_proba'.
-
-        Returns:
-        --------
-        pd.DataFrame
-            Concatenated DataFrame containing predictions made by the model during cross-validation.
-            It includes the original indices joined with the predicted values.
-
-        """
         predictions = []
 
         for train_indices, test_indices in tqdm(self.all_indices):
-            # first drop nas with respect to y_train
-            y_train = y.iloc[train_indices].dropna()
-            # use y_train to filter for X_train
-            X_train = X.iloc[y_train.index]
-            X_test, y_test = X.iloc[test_indices], y.iloc[test_indices]
+            y_train = y[train_indices].dropna()
+            X_train = X[y_train.index]
+            X_test, y_test = X[test_indices], y[test_indices]
 
-            pred = indices.iloc[test_indices].copy()
-            pred['y_true'] = y_test.values
+            pred = {'index': indices[test_indices], 'y_true': y_test}
 
             model = estimator.fit(X_train, y_train)
 
@@ -109,4 +54,4 @@ class PanelSplit:
 
             predictions.append(pred)
 
-        return pd.concat(predictions, axis=0)
+        return predictions
