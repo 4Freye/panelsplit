f1a1275421dbae1e0f9104801a68872ce909ec2a
4Freye <4freye@gmail.com>
2024-05-08 13:32:38 -0700

    updated unit testing

--- tests/test_PanelSplit.py
+++ tests/test_PanelSplit.py
@@ -1,15 +1,14 @@
 # this is still under development
 import unittest
 import pandas as pd
-from ..panelsplit import PanelSplit
+from panelsplit import PanelSplit
 
 class TestPanelSplit(unittest.TestCase):
 
     def setUp(self):
         # Create sample data for testing
-        periods = pd.Series(pd.date_range(start='2022-01-01', end='2022-01-10'))
-        unique_periods = pd.Series([pd.Timestamp('2022-01-01'), pd.Timestamp('2022-01-05'), pd.Timestamp('2022-01-10')])
-        self.panel_split = PanelSplit(periods, unique_periods=unique_periods, n_splits=3)
+        self.periods = pd.Series(pd.date_range(start='2022-01-01', end='2022-01-10').tolist() * 2)
+        self.panel_split = PanelSplit(periods = self.periods, n_splits=3)
 
     def test_split(self):
         # Test if the split method returns the correct number of splits
@@ -23,15 +22,17 @@ class TestPanelSplit(unittest.TestCase):
 
     def test_gen_train_labels(self):
         # Test if the gen_train_labels method returns the correct train labels
-        labels = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        train_labels = self.panel_split.gen_train_labels(labels)
-        self.assertEqual(len(train_labels), 7)  # Assuming one split has no train data
+        vals =[ 0,  1,  2,  3,  4,  5,  6,  7,  8, 10, 11, 12, 13, 14, 15, 16, 17, 18]
+        labels = pd.Series(vals, index= vals)
+        train_labels = self.panel_split.gen_train_labels(pd.Series(range(len(self.periods))))
+        pd.testing.assert_series_equal(train_labels, labels)  # Assuming one split has no train data
 
     def test_gen_test_labels(self):
         # Test if the gen_test_labels method returns the correct test labels
-        labels = pd.Series([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-        test_labels = self.panel_split.gen_test_labels(labels)
-        self.assertEqual(len(test_labels), 3)  # Assuming one split has no test data
+        vals = [ 7,  8,  9, 17, 18, 19]
+        labels = pd.Series(vals, index=vals)
+        test_labels = self.panel_split.gen_test_labels(pd.Series(range(len(self.periods))))
+        pd.testing.assert_series_equal(test_labels, labels)  # Assuming one split has no test data
 
 if __name__ == '__main__':
     unittest.main()
