28f6e3d60ec4c2b6792bf269fdc3b5a2b2a63289
4Freye <4freye@gmail.com>
2025-02-05 11:53:48 -0700

    updating docs

--- docs/conf.py
+++ docs/conf.py
@@ -7,22 +7,23 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'panelsplit'
-copyright = '2025, Eric Frey'
-author = 'Eric Frey'
+copyright = '2025, panelsplit developers'
+author = 'panesplit developers'
 release = '1.0.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.napoleon',  # Optional: for Google/NumPy style docstrings
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = "pydata_sphinx_theme"
 html_static_path = ['_static']
