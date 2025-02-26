aad1c2269256209a37cbf185d79f3cdb6c3e7bb1
4Freye <4freye@gmail.com>
2025-02-26 09:25:26 -0700

    modified init file

--- panelsplit/__init__.py
+++ panelsplit/__init__.py
@@ -5,3 +5,5 @@ panelsplit: a tool for panel data analysis
 PanelSplit is a Python package designed to facilitate time series cross-validation when working with multiple entities (aka panel data). This tool is useful for handling panel data in various stages throughout the data pipeline, including feature engineering, hyper-parameter tuning, and model estimation.
 
 """
+
+__all__ = ["application", "cross_validation", "pipeline", "plot"]
