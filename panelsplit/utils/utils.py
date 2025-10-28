f7f765eb1d2741cfc36f9110b2394ed8b4340154
4Freye <4freye@gmail.com>
2025-10-28 14:14:04 -0700

    added additional type hints for indices argument of split_wrapper function

--- panelsplit/utils/utils.py
+++ panelsplit/utils/utils.py
@@ -1,4 +1,4 @@
-from typing import Union
+from typing import Union, Iterable
 from .typing import CVIndices
 
 from tqdm import tqdm
@@ -8,8 +8,8 @@ __pdoc__ = {"_split_wrapper": False}
 
 
 def _split_wrapper(
-    indices: CVIndices, progress_bar: bool = False
-) -> Union[CVIndices, tqdm]:
+    indices: Union[Iterable, CVIndices], progress_bar: bool = False
+) -> Union[Iterable, CVIndices, tqdm]:
     """Wraps indices with tqdm if progress_bar is True, else returns indices."""
     if progress_bar:
         return tqdm(indices)
